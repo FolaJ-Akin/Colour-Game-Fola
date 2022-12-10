885512e9b7d6bdcd910cc1c840d6648806fbb4e3
FolaJ-Akin <92733148+FolaJ-Akin@users.noreply.github.com>
2022-12-10 18:52:28 +0000

    Update OpenProcessingSpeech.tsx

--- src/component/OpenProcessingSpeech.tsx
+++ src/component/OpenProcessingSpeech.tsx
@@ -14,6 +14,7 @@ export default function OpenProcessingSpeech(): JSX.Element {
     "silver",
     "yellow",
     "gold",
+    "pink",
     "brown",
   ];
   const word = colours[Math.floor(Math.random() * colours.length)];
