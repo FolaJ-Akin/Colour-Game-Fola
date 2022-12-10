95da790913b5c0e962fffaf6f58a04fc92f0bc96
Fola <jimiakinwale@icloud.com>
2022-12-10 18:48:13 +0000

    v1.5 with a choose colour. test of contious display and language to be done

--- src/component/OpenProcessingSpeech.tsx
+++ src/component/OpenProcessingSpeech.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 
 export default function OpenProcessingSpeech(): JSX.Element {
-  if (!window.speechSynthesis) {
-    console.error("Web Speech API not supported");
-  }
+  const colours = [
+    "black",
+    "blue",
+    "grey",
+    "green",
+    "purple",
+    "red",
+    "silver",
+    "yellow",
+    "gold",
+    "brown",
+  ];
+  const word = colours[Math.floor(Math.random() * colours.length)];
+  const [currentColour, setNewColour] = useState(word);
 
   const {
     transcript,
@@ -14,20 +25,31 @@ export default function OpenProcessingSpeech(): JSX.Element {
     // finalTranscript,
     // listening,
     // resetTranscript,
-    // browserSupportsSpeechRecognition,
-    // isMicrophoneAvailable,
+    browserSupportsSpeechRecognition,
+    isMicrophoneAvailable,
   } = useSpeechRecognition();
 
-  //init the speechRecognition engine
-  // const recognition =  SpeechRecognition.getRecognition();
+  if (!browserSupportsSpeechRecognition) {
+    console.error("Web Speech API not supported");
+  }
 
-  // Start listening for speech
-  SpeechRecognition.startListening();
+  if (!isMicrophoneAvailable) {
+    console.error("Web Speech API not supported");
+  }
+
+  SpeechRecognition.startListening({ language: "en", continuous: false });
+
+  function getNewColour() {
+    const newWord = colours[Math.floor(Math.random() * colours.length)];
+    setNewColour(newWord);
+  }
 
   return (
-    <>
+    <div>
       Transcipt: {transcript}
       Interim Transcipt: {interimTranscript}
-    </>
+      <h1>{currentColour}</h1>
+      <button onClick={getNewColour}>Change Colour</button>
+    </div>
   );
 }
